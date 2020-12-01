c3e7a25fca040a99d52b3af706220f9e714fa205
Andrew Whitehead <cywolf@gmail.com>
2020-12-01 11:36:56 -0800

    fix issues with credential definition, schema routes
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- aries_cloudagent/core/conductor.py
+++ aries_cloudagent/core/conductor.py
@@ -31,6 +31,7 @@ from ..transport.outbound.base import OutboundDeliveryError
 from ..transport.outbound.manager import OutboundTransportManager, QueuedOutboundMessage
 from ..transport.outbound.message import OutboundMessage
 from ..transport.wire_format import BaseWireFormat
+from ..wallet.base import DIDInfo
 from ..utils.task_queue import CompletedTask, TaskQueue
 from ..utils.stats import Collector
 
@@ -63,7 +64,7 @@ class Conductor:
         self.inbound_transport_manager: InboundTransportManager = None
         self.outbound_transport_manager: OutboundTransportManager = None
         self.root_profile: Profile = None
-        self.setup_public_did: str = None
+        self.setup_public_did: DIDInfo = None
 
     @property
     def context(self) -> InjectionContext:
@@ -83,7 +84,9 @@ class Conductor:
         context = self.root_profile.context
 
         # Configure the ledger
-        if not await ledger_config(self.root_profile, self.setup_public_did):
+        if not await ledger_config(
+            self.root_profile, self.setup_public_did and self.setup_public_did.did
+        ):
             LOGGER.warning("No ledger configured")
 
         # Register all inbound transports
